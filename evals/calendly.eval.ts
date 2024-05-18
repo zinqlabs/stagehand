1c2103f16e889bf5bb2378e8e368e0d1ce76611d
Paul Klein <paulleoklein@gmail.com>
2024-05-18 14:14:31 -0700

    Add evals

---
+++ evals/calendly.eval.ts
@@ -0,0 +1,24 @@
+import { Eval } from "braintrust";
+import { LevenshteinScorer } from "autoevals";
+import { Stagehand } from "../lib/playwright";
+
+Eval("Calendly", {
+  data: () => {
+    return [
+      {
+        input: "find the calendar",
+        expected: `div[data-testid="calendar"]`,
+      },
+    ];
+  },
+  task: async (input) => {
+    const stageHand = new Stagehand({ env: "LOCAL" });
+    await stageHand.init();
+  
+    await stageHand.page.goto("https://calendly.com/zerostep-test/test-calendly");
+  
+    const result = await stageHand.observe(input);  
+    return stageHand.observations[result].result;
+  },
+  scores: [LevenshteinScorer],
+});
