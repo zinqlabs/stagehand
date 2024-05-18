e046691bcd3a357dd25d44c203b882a98d1b6657
Paul Klein <paulleoklein@gmail.com>
2024-05-18 15:30:16 -0700

    Add Vanta Eval

---
+++ evals/vanta.eval.ts
@@ -0,0 +1,35 @@
+import { Eval } from "braintrust";
+import { Stagehand } from "../lib/playwright";
+
+const exactMatch = (args: { input; output; expected? }) => {
+  return {
+    name: "Exact match",
+    score: args.output === args.expected ? 1 : 0,
+  };
+};
+
+
+Eval("Calendly", {
+  data: () => {
+    return [
+      {
+        input: {text: "find the request demo button", desired: `body > div.page-wrapper > div.nav_component > div.nav_element.w-nav > div.padding-global > div > div > nav > div.nav_cta-wrapper.is-new > a.nav_cta-button-desktop.is-smaller.w-button`},
+        expected: true,
+      },
+    ];
+  },
+  task: async (input) => {
+    const stageHand = new Stagehand({ env: "LOCAL" });
+    await stageHand.init();
+  
+    await stageHand.page.goto("https://www.vanta.com/");
+  
+    const observation = await stageHand.observe(input.text);
+    const observationResult = await stageHand.page.locator(stageHand.observations[observation].result).first().innerHTML();
+    const desiredResult = await stageHand.page.locator(input.desired).first().innerHTML();
+
+    return observationResult == desiredResult;
+  },
+  scores: [exactMatch],
+
+});
