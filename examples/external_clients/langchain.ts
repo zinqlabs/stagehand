32c7c8a42e6c10c491d3d87f244ab5356b6d9874
Filip Michalsky <31483888+filip-michalsky@users.noreply.github.com>
2025-02-13 17:20:06 -0500

    Ishaksebsib/add langchain external client (#484)
    * feat: add LangchainClient extending LLMClient
    
    * eg: add example usage for LangchainClient
    
    * refactor langchain client
    
    * update package.json
    
    * update package
    
    * fix example
    
    * lint
    
    * fix lint
    
    * stub langchain lib types without install
    
    * lint again
    
    * update stub type
    
    * combine modules, return invoke type, response type
    
    * devdependencies
    
    * mv types into sep directory within examples directory
    
    * better example
    
    * prettier
    
    * revert to basemodel
    
    * fix linter and build with basemodel included for langchain
    
    * langchain example
    
    * attempt
    
    * new example
    
    * clean
    
    ---------
    
    Co-authored-by: ishaksebsib <ishaksebsib@gmail.com>
    Co-authored-by: AP <alexphan0515@gmail.com>

---
+++ examples/external_clients/langchain.ts
@@ -0,0 +1,73 @@
+import { BaseChatModel } from "@langchain/core/language_models/chat_models";
+import { ChatGeneration } from "@langchain/core/outputs";
+import { CreateChatCompletionOptions, LLMClient, AvailableModel } from "@/dist";
+import { zodToJsonSchema } from "zod-to-json-schema";
+import {
+  AIMessage,
+  BaseMessageLike,
+  HumanMessage,
+  SystemMessage,
+} from "@langchain/core/messages";
+
+export class LangchainClient extends LLMClient {
+  public type = "langchainClient" as const;
+  private model: BaseChatModel;
+
+  constructor(model: BaseChatModel) {
+    super(model.name as AvailableModel);
+    this.model = model;
+  }
+
+  async createChatCompletion<T = ChatGeneration>({
+    options,
+  }: CreateChatCompletionOptions): Promise<T> {
+    const formattedMessages: BaseMessageLike[] = options.messages.map(
+      (message) => {
+        if (Array.isArray(message.content)) {
+          if (message.role === "system") {
+            return new SystemMessage(
+              message.content
+                .map((c) => ("text" in c ? c.text : ""))
+                .join("\n"),
+            );
+          }
+
+          const content = message.content.map((content) =>
+            "image_url" in content
+              ? { type: "image", image: content.image_url.url }
+              : { type: "text", text: content.text },
+          );
+
+          if (message.role === "user") return new HumanMessage({ content });
+
+          const textOnlyParts = content.map((part) => ({
+            type: "text" as const,
+            text: part.type === "image" ? "[Image]" : part.text,
+          }));
+
+          return new AIMessage({ content: textOnlyParts });
+        }
+
+        return {
+          role: message.role,
+          content: message.content,
+        };
+      },
+    );
+
+    if (options.response_model) {
+      const responseSchema = zodToJsonSchema(options.response_model.schema, {
+        $refStrategy: "none",
+      });
+      const structuredModel = this.model.withStructuredOutput(responseSchema);
+      const response = await structuredModel.invoke(formattedMessages);
+
+      return response as T;
+    }
+
+    const modelWithTools = this.model.bindTools(options.tools);
+    const response = await modelWithTools.invoke(formattedMessages);
+
+    return response as T;
+  }
+}
---
+++ examples/langchain.ts
@@ -0,0 +1,58 @@
+import { z } from "zod";
+import { Stagehand } from "@/dist";
+import StagehandConfig from "@/stagehand.config";
+import { LangchainClient } from "./external_clients/langchain";
+import { ChatOpenAI } from "@langchain/openai";
+
+async function example() {
+  const stagehand = new Stagehand({
+    ...StagehandConfig,
+    llmClient: new LangchainClient(
+      new ChatOpenAI({
+        model: "gpt-4o",
+      }),
+    ),
+  });
+
+  await stagehand.init();
+  await stagehand.page.goto("https://python.langchain.com/docs/introduction/");
+
+  await stagehand.page.waitForTimeout(1000);
+
+  const observation1 = await stagehand.page.observe({
+    instruction: "Go to Conceptual Guides section",
+    returnAction: true,
+  });
+  if (observation1.length > 0) {
+    await stagehand.page.act(observation1[0]);
+  }
+
+  await stagehand.page.waitForTimeout(1000);
+
+  const observation2 = await stagehand.page.observe({
+    instruction: "Click on 'Why LangChain?' located in the content of the page",
+    returnAction: true,
+  });
+  if (observation2.length > 0) {
+    await stagehand.page.act(observation2[0]);
+  }
+
+  await stagehand.page.waitForTimeout(1000);
+
+  const result = await stagehand.page.extract({
+    instruction: "Extract the content of the page",
+    schema: z.object({
+      content: z.string(),
+    }),
+  });
+
+  console.log(result);
+
+  await stagehand.page.waitForTimeout(5000);
+
+  await stagehand.close();
+}
+
+(async () => {
+  await example();
+})();
